41e5af0bdb1393d99c47b1da76603993e5cc815f
Oeslei Kuhn <oesleik@gmail.com>
2016-08-31 21:34:58 -0300

    Login no Google+ com o Oauth.io

--- www/js/index.js
+++ www/js/index.js
@@ -15,7 +15,40 @@ var app = {
     },
 
     onDeviceReady: function() {
-        // code
+        OAuth.initialize('ldh8aAt-ZnZprccwh7ZdtAGTJQw');
+    },
+
+    authenticate: function() {
+        OAuth.popup('google')
+            .then(function(result) {
+                console.info(result);
+
+                result.me()
+                    .then(function(result) {
+                        console.log(result);
+                    });
+            }, function(error) {
+                console.error(error);
+            });
+
+        /*
+        access_token: 'jsdkfjk',
+        expires_in: 3600,
+        id_token: 'jfkdsjfk',
+        provider: 'google',
+        token_type: 'Bearer'
+        */
+
+        /*
+        avatar: 'https://jksdjfkd',
+        email: 'fjsdkfj@jfkd',
+        firstname: 'jfkds',
+        gender: 1,
+        id: 234235423423,
+        lastname: 'jkjk',
+        name: 'jkjfdksfj sdjfkjsd',
+        url: 'https:/jskdf'
+        */
     },
 
 };
