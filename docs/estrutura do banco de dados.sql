6305e4c342707f6b8da12841e2c331bc8851bb5c
Oeslei Kuhn <oesleik@gmail.com>
2016-10-04 20:18:48 -0300

    SQLs com chaves estrangeiras

--- docs/estrutura do banco de dados.sql
+++ docs/estrutura do banco de dados.sql
@@ -1,203 +1,268 @@
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET time_zone = "+00:00";
+SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
+SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
+SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL,ALLOW_INVALID_DATES';
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
-
-CREATE TABLE `autor` (
-  `id` int(10) UNSIGNED NOT NULL,
-  `nome` varchar(120) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
-CREATE TABLE `categoria` (
-  `id` int(10) UNSIGNED NOT NULL,
-  `nome` varchar(60) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
-CREATE TABLE `comentario_aprovacao` (
-  `id` int(10) UNSIGNED NOT NULL,
-  `id_comentario` int(10) UNSIGNED NOT NULL,
-  `id_usuario` int(10) UNSIGNED NOT NULL,
-  `aprovacao` int(1) NOT NULL DEFAULT '0'
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
-CREATE TABLE `comentario_moderacao` (
-  `id` int(10) UNSIGNED NOT NULL,
-  `id_comentario` int(10) UNSIGNED NOT NULL,
-  `id_usuario` int(10) UNSIGNED NOT NULL,
-  `tipo_comentario` varchar(10) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
-CREATE TABLE `login` (
-  `id` int(10) UNSIGNED NOT NULL,
-  `id_usuario` int(10) UNSIGNED NOT NULL,
-  `id_perfil_social` int(10) UNSIGNED NOT NULL,
-  `token` varchar(250) NOT NULL,
-  `expires_in` varchar(60) DEFAULT NULL,
-  `refresh_token` varchar(250) DEFAULT NULL,
-  `token_id` varchar(80) DEFAULT NULL,
-  `token_type` varchar(40) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
-CREATE TABLE `perfil_social` (
-  `id` int(10) UNSIGNED NOT NULL,
-  `id_usuario` int(10) UNSIGNED NOT NULL,
-  `id_usuario_rede` varchar(120) NOT NULL,
-  `email` varchar(120) NOT NULL,
-  `avatar` varchar(250) NOT NULL,
-  `url` varchar(250) NOT NULL,
-  `rede_social` varchar(40) NOT NULL,
-  `exibir` tinyint(1) NOT NULL DEFAULT '1'
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
-CREATE TABLE `prateleira` (
-  `id` int(10) UNSIGNED NOT NULL,
-  `id_usuario` int(10) UNSIGNED NOT NULL,
-  `nome` varchar(60) NOT NULL,
-  `publica` tinyint(1) NOT NULL DEFAULT '1'
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
-CREATE TABLE `prateleira_volume` (
-  `id` int(10) UNSIGNED NOT NULL,
-  `id_prateleira` int(10) UNSIGNED NOT NULL,
-  `id_volume` int(10) UNSIGNED NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
-CREATE TABLE `usuario` (
-  `id` int(10) UNSIGNED NOT NULL,
-  `nome` varchar(60) NOT NULL,
-  `sobrenome` varchar(80) NOT NULL,
-  `apelido` varchar(60) NOT NULL,
-  `email` varchar(120) NOT NULL,
-  `avatar` varchar(250) NOT NULL,
-  `exibir_email` tinyint(1) NOT NULL DEFAULT '0'
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
-CREATE TABLE `volume` (
-  `id` int(10) UNSIGNED NOT NULL,
-  `titulo` varchar(250) NOT NULL,
-  `subtitulo` varchar(250) NOT NULL,
-  `isbn` varchar(14) DEFAULT NULL,
-  `paginas` int(11) DEFAULT NULL,
-  `linguagem` varchar(60) DEFAULT NULL,
-  `dataPublicacao` date DEFAULT NULL,
-  `id_google` varchar(60) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
-CREATE TABLE `volume_autor` (
-  `id` int(10) UNSIGNED NOT NULL,
-  `id_volume` int(10) UNSIGNED NOT NULL,
-  `id_autor` int(10) UNSIGNED NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
-CREATE TABLE `volume_categoria` (
-  `id` int(10) UNSIGNED NOT NULL,
-  `id_volume` int(10) UNSIGNED NOT NULL,
-  `id_categoria` int(10) UNSIGNED NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
-CREATE TABLE `volume_comentario` (
-  `id` int(10) UNSIGNED NOT NULL,
-  `id_volume` int(10) UNSIGNED NOT NULL,
-  `comentario` text NOT NULL,
-  `id_comentario` int(10) UNSIGNED DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
-CREATE TABLE `volume_imagem` (
-  `id` int(10) UNSIGNED NOT NULL,
-  `id_volume` int(10) UNSIGNED NOT NULL,
-  `caminho` varchar(250) NOT NULL,
-  `tamanho` varchar(10) NOT NULL DEFAULT 'normal'
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
-CREATE TABLE `volume_usuario` (
-  `id` int(10) UNSIGNED NOT NULL,
-  `id_volume` int(10) UNSIGNED NOT NULL,
-  `id_usuario` int(10) UNSIGNED NOT NULL,
-  `situacao` varchar(10) NOT NULL,
-  `avaliacao` int(2) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+CREATE TABLE IF NOT EXISTS `autor` (
+  `id` INT(10) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `nome` VARCHAR(120) NOT NULL,
+  PRIMARY KEY (`id`))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8;
 
 
-ALTER TABLE `autor`
-  ADD PRIMARY KEY (`id`);
-
-ALTER TABLE `categoria`
-  ADD PRIMARY KEY (`id`);
-
-ALTER TABLE `comentario_aprovacao`
-  ADD PRIMARY KEY (`id`);
-
-ALTER TABLE `comentario_moderacao`
-  ADD PRIMARY KEY (`id`);
-
-ALTER TABLE `login`
-  ADD PRIMARY KEY (`id`);
-
-ALTER TABLE `perfil_social`
-  ADD PRIMARY KEY (`id`);
-
-ALTER TABLE `prateleira`
-  ADD PRIMARY KEY (`id`);
-
-ALTER TABLE `prateleira_volume`
-  ADD PRIMARY KEY (`id`);
-
-ALTER TABLE `usuario`
-  ADD PRIMARY KEY (`id`);
-
-ALTER TABLE `volume`
-  ADD PRIMARY KEY (`id`);
-
-ALTER TABLE `volume_autor`
-  ADD PRIMARY KEY (`id`);
-
-ALTER TABLE `volume_categoria`
-  ADD PRIMARY KEY (`id`);
-
-ALTER TABLE `volume_comentario`
-  ADD PRIMARY KEY (`id`);
-
-ALTER TABLE `volume_imagem`
-  ADD PRIMARY KEY (`id`);
-
-ALTER TABLE `volume_usuario`
-  ADD PRIMARY KEY (`id`);
+CREATE TABLE IF NOT EXISTS `categoria` (
+  `id` INT(10) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `nome` VARCHAR(60) NOT NULL,
+  PRIMARY KEY (`id`))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8;
 
 
-ALTER TABLE `autor`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
-ALTER TABLE `categoria`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
-ALTER TABLE `comentario_aprovacao`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
-ALTER TABLE `comentario_moderacao`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
-ALTER TABLE `login`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
-ALTER TABLE `perfil_social`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
-ALTER TABLE `prateleira`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
-ALTER TABLE `prateleira_volume`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
-ALTER TABLE `usuario`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
-ALTER TABLE `volume`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
-ALTER TABLE `volume_autor`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
-ALTER TABLE `volume_categoria`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
-ALTER TABLE `volume_comentario`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
-ALTER TABLE `volume_imagem`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
-ALTER TABLE `volume_usuario`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+CREATE TABLE IF NOT EXISTS `usuario` (
+  `id` INT(10) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `nome` VARCHAR(60) NOT NULL,
+  `sobrenome` VARCHAR(80) NOT NULL,
+  `apelido` VARCHAR(60) NOT NULL,
+  `email` VARCHAR(120) NOT NULL,
+  `avatar` VARCHAR(250) NOT NULL,
+  `exibir_email` TINYINT(1) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8;
+
+
+CREATE TABLE IF NOT EXISTS `volume` (
+  `id` INT(10) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `titulo` VARCHAR(250) NOT NULL,
+  `subtitulo` VARCHAR(250) NOT NULL,
+  `isbn` VARCHAR(14) NULL DEFAULT NULL,
+  `paginas` INT(11) NULL DEFAULT NULL,
+  `linguagem` VARCHAR(60) NULL DEFAULT NULL,
+  `dataPublicacao` DATE NULL DEFAULT NULL,
+  `id_google` VARCHAR(60) NOT NULL,
+  PRIMARY KEY (`id`))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8;
+
+
+CREATE TABLE IF NOT EXISTS `volume_comentario` (
+  `id` INT(10) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `id_volume` INT(10) UNSIGNED NOT NULL,
+  `comentario` TEXT NOT NULL,
+  `id_comentario` INT(10) UNSIGNED NULL DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  INDEX `fk_id_volume_idx` (`id_volume` ASC),
+  CONSTRAINT `fk_id_volume`
+    FOREIGN KEY (`id_volume`)
+    REFERENCES `volume` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8;
+
+
+CREATE TABLE IF NOT EXISTS `comentario_aprovacao` (
+  `id` INT(10) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `id_comentario` INT(10) UNSIGNED NOT NULL,
+  `id_usuario` INT(10) UNSIGNED NOT NULL,
+  `aprovacao` INT(1) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`),
+  INDEX `fk_id_ususario_idx` (`id_usuario` ASC),
+  INDEX `fk_id_comentario_idx` (`id_comentario` ASC),
+  CONSTRAINT `fk_id_ususario`
+    FOREIGN KEY (`id_usuario`)
+    REFERENCES `usuario` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_id_comentario`
+    FOREIGN KEY (`id_comentario`)
+    REFERENCES `volume_comentario` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8;
+
+
+CREATE TABLE IF NOT EXISTS `comentario_moderacao` (
+  `id` INT(10) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `id_comentario` INT(10) UNSIGNED NOT NULL,
+  `id_usuario` INT(10) UNSIGNED NOT NULL,
+  `tipo_comentario` VARCHAR(10) NOT NULL,
+  PRIMARY KEY (`id`),
+  INDEX `fk_id_usuario_idx` (`id_usuario` ASC),
+  INDEX `fk_id_comentario_idx` (`id_comentario` ASC),
+  CONSTRAINT `fk_id_usuario`
+    FOREIGN KEY (`id_usuario`)
+    REFERENCES `usuario` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_id_comentario_mod`
+    FOREIGN KEY (`id_comentario`)
+    REFERENCES `volume_comentario` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8;
+
+
+CREATE TABLE IF NOT EXISTS `perfil_social` (
+  `id` INT(10) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `id_usuario` INT(10) UNSIGNED NOT NULL,
+  `id_usuario_rede` VARCHAR(120) NOT NULL,
+  `email` VARCHAR(120) NOT NULL,
+  `avatar` VARCHAR(250) NOT NULL,
+  `url` VARCHAR(250) NOT NULL,
+  `rede_social` VARCHAR(40) NOT NULL,
+  `exibir` TINYINT(1) NOT NULL DEFAULT '1',
+  PRIMARY KEY (`id`))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8;
+
+
+CREATE TABLE IF NOT EXISTS `login` (
+  `id` INT(10) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `id_usuario` INT(10) UNSIGNED NOT NULL,
+  `id_perfil_social` INT(10) UNSIGNED NOT NULL,
+  `token` VARCHAR(250) NOT NULL,
+  `expires_in` VARCHAR(60) NULL DEFAULT NULL,
+  `refresh_token` VARCHAR(250) NULL DEFAULT NULL,
+  `token_id` VARCHAR(80) NULL DEFAULT NULL,
+  `token_type` VARCHAR(40) NULL DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  INDEX `fk_id_usuario_idx` (`id_usuario` ASC),
+  INDEX `fk_id_perfil_social_idx` (`id_perfil_social` ASC),
+  CONSTRAINT `fk_id_usuario_login`
+    FOREIGN KEY (`id_usuario`)
+    REFERENCES `usuario` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_id_perfil_social`
+    FOREIGN KEY (`id_perfil_social`)
+    REFERENCES `perfil_social` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8;
+
+
+CREATE TABLE IF NOT EXISTS `prateleira` (
+  `id` INT(10) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `id_usuario` INT(10) UNSIGNED NOT NULL,
+  `nome` VARCHAR(60) NOT NULL,
+  `publica` TINYINT(1) NOT NULL DEFAULT '1',
+  PRIMARY KEY (`id`),
+  INDEX `fk_id_usuario_idx` (`id_usuario` ASC),
+  CONSTRAINT `fk_id_usuario_prat`
+    FOREIGN KEY (`id_usuario`)
+    REFERENCES `usuario` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8;
+
+
+CREATE TABLE IF NOT EXISTS `prateleira_volume` (
+  `id` INT(10) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `id_prateleira` INT(10) UNSIGNED NOT NULL,
+  `id_volume` INT(10) UNSIGNED NOT NULL,
+  PRIMARY KEY (`id`),
+  INDEX `fk_id_volume_idx` (`id_volume` ASC),
+  INDEX `fk_id_prateleira_idx` (`id_prateleira` ASC),
+  CONSTRAINT `fk_id_prateleira`
+    FOREIGN KEY (`id_prateleira`)
+    REFERENCES `prateleira` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_id_volume_prat`
+    FOREIGN KEY (`id_volume`)
+    REFERENCES `volume` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8;
+
+
+CREATE TABLE IF NOT EXISTS `volume_autor` (
+  `id` INT(10) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `id_volume` INT(10) UNSIGNED NOT NULL,
+  `id_autor` INT(10) UNSIGNED NOT NULL,
+  PRIMARY KEY (`id`),
+  INDEX `fk_id_autor_idx` (`id_autor` ASC),
+  INDEX `fk_id_volume_idx` (`id_volume` ASC),
+  CONSTRAINT `fk_id_autor`
+    FOREIGN KEY (`id_autor`)
+    REFERENCES `autor` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_id_volume_aut`
+    FOREIGN KEY (`id_volume`)
+    REFERENCES `volume` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8;
+
+
+CREATE TABLE IF NOT EXISTS `volume_categoria` (
+  `id` INT(10) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `id_volume` INT(10) UNSIGNED NOT NULL,
+  `id_categoria` INT(10) UNSIGNED NOT NULL,
+  PRIMARY KEY (`id`),
+  INDEX `fk_id_volume_idx` (`id_volume` ASC),
+  INDEX `fk_id_categoria_idx` (`id_categoria` ASC),
+  CONSTRAINT `fk_id_volume_cat`
+    FOREIGN KEY (`id_volume`)
+    REFERENCES `volume` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_id_categoria`
+    FOREIGN KEY (`id_categoria`)
+    REFERENCES `categoria` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8;
+
+
+CREATE TABLE IF NOT EXISTS `volume_imagem` (
+  `id` INT(10) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `id_volume` INT(10) UNSIGNED NOT NULL,
+  `caminho` VARCHAR(250) NOT NULL,
+  `tamanho` VARCHAR(10) NOT NULL DEFAULT 'normal',
+  PRIMARY KEY (`id`),
+  INDEX `fk_id_volume_idx` (`id_volume` ASC),
+  CONSTRAINT `fk_id_volume_img`
+    FOREIGN KEY (`id_volume`)
+    REFERENCES `volume` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8;
+
+
+CREATE TABLE IF NOT EXISTS `volume_usuario` (
+  `id` INT(10) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `id_volume` INT(10) UNSIGNED NOT NULL,
+  `id_usuario` INT(10) UNSIGNED NOT NULL,
+  `situacao` VARCHAR(10) NOT NULL,
+  `avaliacao` INT(2) NOT NULL,
+  PRIMARY KEY (`id`),
+  INDEX `fk_id_usuario_idx` (`id_usuario` ASC),
+  INDEX `fk_id_volume_idx` (`id_volume` ASC),
+  CONSTRAINT `fk_id_usuario_vol`
+    FOREIGN KEY (`id_usuario`)
+    REFERENCES `usuario` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_id_volume_usuario`
+    FOREIGN KEY (`id_volume`)
+    REFERENCES `volume` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8;
+
+
+SET SQL_MODE=@OLD_SQL_MODE;
+SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
+SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
