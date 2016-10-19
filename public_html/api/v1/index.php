ab30416655113540c380c8f8a4f4b077398ac678
Juliano Gatto <julianogattodropbox@gmail.com>
2016-10-19 13:10:36 -0200

    Update index.php

--- public_html/api/v1/index.php
+++ public_html/api/v1/index.php
@@ -13,4 +13,9 @@ $app->get("/usuarios", "Services\Usuarios:getAll");
 $app->get("/usuarios/{id}", "Services\Usuarios:get");
 $app->post("/usuarios", "Services\Usuarios:add");
 
+// Volume
+$app->get("/volumes", "Services\Volumes:getAll");
+$app->get("/volumes/{id}", "Services\Volumes:get");
+$app->post("/volumes", "Services\Volumes:add");
+
 $app->run();
