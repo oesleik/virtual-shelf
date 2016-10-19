0401123ac6be7baed943dbea8b77ced810eb5ef9
Juliano Gatto <julianogattodropbox@gmail.com>
2016-10-19 13:08:53 -0200

    Add files via upload

---
+++ public_html/api/v1/Services/Volumes.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Services;
+
+use Models\Volume;
+
+class Volumes extends Services {
+
+	public function getAll($req, $res) {
+		$volumes = Volume::all();
+		return $this->parseResponse($res, $volumes);
+	}
+
+	public function get($req, $res) {
+		$id = $req->getAttribute("id");
+		$volume = Volume::find($id);
+
+		if ($volume === null) {
+			return $this->parseResponse($res, "Volume não encontrado", self::ERROR);
+		} else {
+			return $this->parseResponse($res, $volume);
+		}
+	}
+
+	public function add($req, $res) {
+		$dados = $req->getBody();
+		return $this->parseResponse($res, $dados);
+
+	}
+
+	public function edit($req, $res) {
+
+	}
+
+	public function delete($req, $res) {
+
+	}
+
+}
