c641afcd2884e7beeba33900f6104373771da5a6
Juliano Gatto <julianogattodropbox@gmail.com>
2016-10-26 20:37:32 -0200

    adicionado model prateleira

---
+++ public_html/api/v1/Models/Prateleira.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Models;
+
+use \Illuminate\Database\Eloquent\Model;
+
+class Prateleira extends Model {
+
+	protected $table = "prateleira";
+	protected $guarded = [];
+
+	public $timestamps = false;
+
+}
