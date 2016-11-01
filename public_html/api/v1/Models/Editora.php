67afb3b11bdff8768bbb45509dd000d36420cd1f
Juliano Gatto <julianogattodropbox@gmail.com>
2016-11-01 20:37:42 -0200

    model editora

---
+++ public_html/api/v1/Models/Editora.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Models;
+
+use \Illuminate\Database\Eloquent\Model;
+
+class Editora extends Model {
+
+	protected $table = "editora";
+	protected $guarded = [];
+
+	public $timestamps = false;
+
+}
