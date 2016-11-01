86b0aefa0569fb8c909b32a6be1aa0d42e9b30e5
Juliano Gatto <julianogattodropbox@gmail.com>
2016-11-01 21:07:04 -0200

    model autor

---
+++ public_html/api/v1/Models/Autor.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Models;
+
+use \Illuminate\Database\Eloquent\Model;
+
+class Autor extends Model {
+
+	protected $table = "autor";
+	protected $guarded = [];
+
+	public $timestamps = false;
+
+}
