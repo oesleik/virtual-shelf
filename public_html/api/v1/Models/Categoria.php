7c35e6f752dd9a53b20ffb4a8d846847401916b6
Juliano Gatto <julianogattodropbox@gmail.com>
2016-11-01 21:07:14 -0200

    model categoria

---
+++ public_html/api/v1/Models/Categoria.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Models;
+
+use \Illuminate\Database\Eloquent\Model;
+
+class Categoria extends Model {
+
+	protected $table = "categoria";
+	protected $guarded = [];
+
+	public $timestamps = false;
+
+}
