6a9947aa312e946993309f8df79e67e21b42114e
Oeslei Kuhn <oesleik@gmail.com>
2016-11-09 20:43:05 -0200

    API Volumes: pesquisa de livros

--- public_html/api/v1/Models/VolumeImagem.php
+++ public_html/api/v1/Models/VolumeImagem.php
@@ -6,6 +6,9 @@ use \Illuminate\Database\Eloquent\Model;
 
 class VolumeImagem extends Model {
 
+	const TAMANHO_THUMB = "thumb";
+	const TAMANHO_THUMB_SM = "thumb_sm";
+
 	protected $table = "volume_imagem";
 	protected $guarded = [];
 
--- public_html/api/v1/Services/Volumes.php
+++ public_html/api/v1/Services/Volumes.php
@@ -3,6 +3,12 @@
 namespace Services;
 
 use Models\Volume;
+use Models\Editora;
+use Models\Autor;
+use Models\VolumeAutor;
+use Models\Categoria;
+use Models\VolumeCategoria;
+use Models\VolumeImagem;
 
 class Volumes extends Services {
 
@@ -23,8 +29,16 @@ class Volumes extends Services {
 	}
 
 	public function getPesquisa($req, $res) {
-		$id = $req->getAttribute("pesquisa");
-		$volumes = Volume::all();
+		$pesquisa = $req->getAttribute("pesquisa");
+
+		$service = \Utils\getGoogleBooksService();
+		$response = $service->volumes->listVolumes($pesquisa);
+
+		$volumes = array();
+		foreach ($response as $item) {
+			$volumes[] = $this->_parseVolumeGoogle($item);
+		}
+
 		return $this->parseResponse($res, $volumes);
 	}
 
@@ -48,6 +62,72 @@ class Volumes extends Services {
 		$id = $req->getAttribute("id");
 		$deleted = (bool) Volume::destroy($id);
 		return $this->parseResponse($res, $deleted);
+	}
+
+	protected _parseVolumeGoogle($item) {
+		$related = array();
+		$info = $item["volumeInfo"];
+
+		$editora = Editora::firstOrCreate([
+			"nome" => $info["publisher"]
+		]);
+		$related["editora"] = $editora;
+
+		$volume = Volume::firstOrCreate([
+			"titulo" => $info["title"],
+			"isbn" => $info["industryIdentifiers"][0]["identifier"],
+			"paginas" => $info["pageCount"],
+			"linguagem" => $info["language"],
+			"dataPublicacao" => $info["publishedDate"],
+			"id_google" => $item["id"],
+			"id_editora" => $editora->id
+		]);
+
+		$related["autores"] = array();
+		foreach ($info["authors"] as $nome) {
+			$autor = Autor::firstOrCreate([
+				"nome" => $nome
+			]);
+			$related["autores"][] = $autor;
+
+			VolumeAutor::firstOrCreate([
+				"id_volume" => $volume->id,
+				"id_autor" => $autor->id
+			]);
+		}
+
+		$related["categorias"] = array();
+		foreach ($info["categories"] as $nome) {
+			$categoria = Categoria::firstOrCreate([
+				"nome" => $nome
+			]);
+			$related["categorias"][] = $categoria;
+
+			VolumeCategoria::firstOrCreate([
+				"id_volume" => $volume->id,
+				"id_categoria" => $categoria->id
+			]);
+		}
+
+		$related["imagens"] = array();
+		if ($info["imageLinks"]["thumbnail"]) {
+			$imagem = VolumeImagem::firstOrCreate([
+				"id_volume" => $volume->id,
+				"tamanho" => VolumeImagem::TAMANHO_THUMB,
+				"caminho" => $info["imageLinks"]["thumbnail"]
+			]);
+			$related["imagens"][VolumeImagem::TAMANHO_THUMB] = $imagem;
+		}
+		if ($info["imageLinks"]["smallThumbnail"]) {
+			$imagem = VolumeImagem::firstOrCreate([
+				"id_volume" => $volume->id,
+				"tamanho" => VolumeImagem::TAMANHO_THUMB_SM,
+				"caminho" => $info["imageLinks"]["smallThumbnail"]
+			]);
+			$related["imagens"][VolumeImagem::TAMANHO_THUMB_SM] = $imagem;
+		}
+
+		return array_merge($volume->toArray(), $related);
 	}
 
 }
