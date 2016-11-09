055750c869b304ff1f215bdf8428cb5ed494c977
Juliano Gatto <julianogattodropbox@gmail.com>
2016-11-09 20:57:43 -0200

    Model VolumeUsuario

---
+++ public_html/api/v1/Models/VolumeUsuario.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Models;
+
+use \Illuminate\Database\Eloquent\Model;
+
+class VolumeUsuario extends Model {
+
+	protected $table = "volume_usuario";
+	protected $guarded = [];
+
+	public $timestamps = false;
+
+}
