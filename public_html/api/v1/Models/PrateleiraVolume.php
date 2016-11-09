53450370a308086c26c11f17a60ee1903ce7464a
Juliano Gatto <julianogattodropbox@gmail.com>
2016-11-09 20:57:01 -0200

    Model PrateleiraVolume

---
+++ public_html/api/v1/Models/PrateleiraVolume.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Models;
+
+use \Illuminate\Database\Eloquent\Model;
+
+class PrateleiraVolume extends Model {
+
+	protected $table = "prateleira_volume";
+	protected $guarded = [];
+
+	public $timestamps = false;
+
+}
