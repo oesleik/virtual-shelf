744649779fb8e8579b02c7b6ae9db4b4a900a4cc
Juliano Gatto <julianogattodropbox@gmail.com>
2016-11-09 20:57:32 -0200

    Model VolumeCategoria

---
+++ public_html/api/v1/Models/VolumeCategoria.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Models;
+
+use \Illuminate\Database\Eloquent\Model;
+
+class VolumeCategoria extends Model {
+
+	protected $table = "volume_categoria";
+	protected $guarded = [];
+
+	public $timestamps = false;
+
+}
