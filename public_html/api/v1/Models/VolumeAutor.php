d59f784977ce420f9fb54ed3122671982ffdb0c1
Juliano Gatto <julianogattodropbox@gmail.com>
2016-11-09 20:57:11 -0200

    Model VolumeAutor

---
+++ public_html/api/v1/Models/VolumeAutor.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Models;
+
+use \Illuminate\Database\Eloquent\Model;
+
+class VolumeAutor extends Model {
+
+	protected $table = "volume_autor";
+	protected $guarded = [];
+
+	public $timestamps = false;
+
+}
