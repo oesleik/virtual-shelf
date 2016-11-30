1b34db20e14130392f888ae6f5a70dc61ec173a4
Oeslei Kuhn <oesleik@gmail.com>
2016-11-30 20:23:48 -0200

    Listagem de livros (pesquisa)

--- www/js/elements/volume-box.js
+++ www/js/elements/volume-box.js
@@ -11,13 +11,15 @@
 					<img src="${this.volume.imagens.thumb_sm.caminho}" />
 					<h4>${this.volume.titulo}</h4>
 					<h5>${this.volume.autores.map((autor) => autor.nome).join(", ")}</h5>
-					<div class="status">Lido</div>
-					<div class="rating">
-						<i class="material-icons">star</i>
-						<i class="material-icons">star</i>
-						<i class="material-icons">star</i>
-						<i class="material-icons">star</i>
-						<i class="material-icons">star</i>
+					<div class="secondary">
+						<div class="status">Lido <i class="material-icons">arrow_drop_down</i></div>
+						<div class="rating">
+							<i class="material-icons">star</i>
+							<i class="material-icons">star</i>
+							<i class="material-icons">star</i>
+							<i class="material-icons">star</i>
+							<i class="material-icons">star</i>
+						</div>
 					</div>
 					<div class="clearfix"></div>`
 				);
@@ -30,6 +32,7 @@
 
 	restyle({
 		"volume-box": {
+			"position": "relative",
 			"margin": "15px",
 			"display": "block"
 		},
@@ -40,14 +43,16 @@
 			"height": "150px"
 		},
 		"volume-box h4": {
-			"display": "block",
-			"white-space": "nowrap",
+			"display": "-webkit-box",
+			"-webkit-box-orient": "vertical",
+			"-webkit-line-clamp": "2",
 			"overflow": "hidden",
 			"text-overflow": "ellipsis",
 			"margin": "0",
-			"padding": "5px 0",
+			"padding": "5px 0 0",
 			"font-size": "20px",
-			"line-height": "20px"
+			"line-height": "20px",
+			"max-height": "40px"
 		},
 		"volume-box h5": {
 			"display": "block",
@@ -55,10 +60,22 @@
 			"overflow": "hidden",
 			"text-overflow": "ellipsis",
 			"margin": "0",
-			"padding": "0 0 10px",
+			"padding": "8px 0 10px",
 			"font-size": "16px",
 			"line-height": "16px",
 			"color": "#999"
+		},
+		"volume-box .secondary": {
+			"position": "absolute",
+			"bottom": "3px",
+			"left": "110px"
+		},
+		"volume-box .status": {
+			"font-size": "15px",
+			"color": "#666"
+		},
+		"volume-box .status i": {
+			"vertical-align": "bottom"
 		}
 	}, []);
 
